f75c442e82627748e31a81223673041d01351ed1
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-04-20 18:31:44 -0400

    manage search history

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -28,6 +28,12 @@ export default function Root() {
   const navigation = useNavigation()
   const submit = useSubmit()
 
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    )
+
   useEffect(() => {
     document.getElementById("q").value = q
   }, [q])
@@ -40,16 +46,23 @@ export default function Root() {
         <Form id="search-form" role="search">
           <input
             id="q"
+            className={searching ? "loading" : ""}
             aria-label="Search contacts"
             placeholder="Search"
             type="search"
             name="q"
             defaultValue={q}
             onChange={(event) => { // search bar automatically populates list based on what you type in
-              submit(event.currentTarget.form)
+              const isFirstSearch = q == null // manages history stack, makes it so site does not remember all search results in history?
+              submit(event.currentTarget.form, {
+                replace: !isFirstSearch,
+              })
             }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
+            <div 
+              id="search-spinner" 
+              aria-hidden 
+              hidden={!searching} />
             <div className="sr-only" aria-live="polite"></div>
         </Form>
         <Form method="post">
